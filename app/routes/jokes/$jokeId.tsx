82a9469bb778c10ea7fd6ecf84b660b1ad491982
Zaeem0 <zaeem.q97@gmail.com>
2021-12-07 21:52:29 +0000

    prefetching and optimistic UI

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,5 +1,6 @@
 import type { Joke } from "@prisma/client";
-import { ActionFunction, Form, Link, LoaderFunction, MetaFunction, redirect, useCatch, useLoaderData, useParams } from "remix";
+import { ActionFunction, LoaderFunction, MetaFunction, redirect, useCatch, useLoaderData, useParams } from "remix";
+import { JokeDisplay } from "~/components/joke";
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
 
@@ -69,23 +70,7 @@ export default function JokeRoute() {
   const data = useLoaderData<LoaderData>();
 
   return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
-      {data.isOwner ? (
-        <Form method="post">
-          <input
-            type="hidden"
-            name="_method"
-            value="delete"
-          />
-          <button type="submit" className="button">
-            Delete
-          </button>
-        </Form>
-      ) : null}
-    </div>
+      <JokeDisplay joke={data.joke} isOwner={data.isOwner} />
   );
 }
 
--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,4 +1,5 @@
-import { ActionFunction, Form, json, Link, LoaderFunction, redirect, useActionData, useCatch } from "remix";
+import { ActionFunction, Form, json, Link, LoaderFunction, redirect, useActionData, useCatch, useTransition } from "remix";
+import { JokeDisplay } from "~/components/joke";
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
 
@@ -68,7 +69,27 @@ export const action: ActionFunction = async ({
 
 export default function NewJokeRoute() {
   const actionData = useActionData<ActionData>();
+  const transition = useTransition();
 
+  if (transition.submission) {
+    const name = transition.submission.formData.get("name");
+    const content =
+      transition.submission.formData.get("content");
+    if (
+      typeof name === "string" &&
+      typeof content === "string" &&
+      !validateJokeContent(content) &&
+      !validateJokeName(name)
+    ) {
+      return (
+        <JokeDisplay
+          joke={{ name, content }}
+          isOwner={true}
+          canDelete={false}
+        />
+      );
+    }
+  }
   return (
     <div>
       <p>Add your own hilarious joke</p>
